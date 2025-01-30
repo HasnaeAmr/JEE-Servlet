db9841698ad9ab8ed00b733e9f703b5b86be8831
Hasnae Amr <150762808+HasnaeAmr@users.noreply.github.com>
2025-01-30 23:36:39 +0100

    Update Imc.java

--- src/main/java/web/Imc.java
+++ src/main/java/web/Imc.java
@@ -1,18 +1,30 @@
 package web;
 
 public class Imc {
-	float taille;
-	float poids;
-	
-	public Imc(float taille, float poids) {
-		this.taille=taille;
-		this.poids=poids;
-	}
-	
-	float calcul() {
-		return poids / (taille * taille);
-	}
+    float taille;
+    float poids;
 
+    public Imc(float taille, float poids) {
+        this.taille = taille;
+        this.poids = poids;
+    }
+
+    // Calculate the BMI
+    float calcul() {
+        return poids / (taille * taille);
+    }
+
+    // Determine the category based on the BMI value
+    public String getCategory() {
+        float imc = calcul();
+        if (imc < 18.5) {
+            return "Insuffisance pondérale";  // Underweight
+        } else if (imc < 24.9) {
+            return "Poids normal";  // Normal weight
+        } else if (imc < 29.9) {
+            return "Surpoids";  // Overweight
+        } else {
+            return "Obésité";  // Obesity
+        }
+    }
 }
-
-
